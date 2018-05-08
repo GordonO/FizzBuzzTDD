b11746412b748c8a6d188f65ff5c0f4e88857af5
ONeill_Desk <ONeill_Desk@ONeill_Desk-PC>
2018-05-08 00:17:01 -0400

    Refactored variable names.

--- FizzBuzzTDD/src/main/FizzBuzz.java
+++ FizzBuzzTDD/src/main/FizzBuzz.java
@@ -2,29 +2,30 @@ package main;
 
 public class FizzBuzz {
 	
-	public String decision(Integer number){
+	public String decision(Integer pInputNumber){
 		
 		String sFizz, sBuzz, sDecision;
 		
 		sDecision = "";
-		sFizz = fizz(number);
-		sBuzz = buzz(number);
+		sFizz = fizz(pInputNumber);
+		sBuzz = buzz(pInputNumber);
 		
 		if(sFizz == "Fizz") sDecision = sFizz;
 		if(sBuzz == "Buzz") sDecision = sDecision + sBuzz;
-		if(sFizz == "No" & sBuzz == "No") sDecision = number.toString();
+		if(sFizz == "No" & sBuzz == "No") sDecision = pInputNumber.toString();
 		
 		return sDecision;
 	}
-	public String fizz(int number){
+	
+	public String fizz(int pFizzNumber){
 		
-		if(number % 3 == 0) return "Fizz";
+		if(pFizzNumber % 3 == 0) return "Fizz";
 		else return "No";
 	}
 	
-	public String buzz(int number){
+	public String buzz(int pBuzzNumber){
 		
-		if(number % 5 == 0) return "Buzz";
+		if(pBuzzNumber % 5 == 0) return "Buzz";
 		else return "No";
 	}
 }
